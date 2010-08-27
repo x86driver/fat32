cd0fbd2b9faa4afc9a92a488ddedd9328fd47474
doremi <nothing>
2010-08-27 14:31:22 +0800

    可以把檔案列出 然後印出該檔案會用到哪幾個 cluster

--- read.c
+++ read.c
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include "fat32.h"
 
+#define SECTOR_SIZE 512
+
 unsigned char *buf;
 
 struct Partition {
@@ -25,8 +27,8 @@ struct Partition {
 /*
 void read_sec(unsigned int lba)
 {
-	lseek(fd, lba*512, SEEK_SET);
-	read(fd, (void*)&sector[0], 512);
+	lseek(fd, lba*SECTOR_SIZE, SEEK_SET);
+	read(fd, (void*)&sector[0], SECTOR_SIZE);
 }
 */
 
@@ -46,7 +48,7 @@ static inline unsigned int find_first_partition()
 			partition->endhead, partition->endsector, partition->endcylinder);
 		printf("LBA Start: %d, total: %d, size: %d (%d)\n\n",
 			partition->startlba, partition->totalsec,
-			partition->totalsec*512, (partition->totalsec*512)/1048576);
+			partition->totalsec*SECTOR_SIZE, (partition->totalsec*SECTOR_SIZE)/1048576);
 		printf("===============================================\n\n");
 		partition += sizeof(struct Partition);
 	}
@@ -57,11 +59,12 @@ struct FAT32 *fat;
 unsigned int FATSz;
 unsigned int fat_table;
 unsigned int RootDirSectors;
+unsigned int FirstFatSector;
 unsigned int FirstDataSector;
 
 void get_fat_info(unsigned int fat32_sec)
 {
-	fat = (struct FAT32*)(buf+(fat32_sec*512));
+	fat = (struct FAT32*)(buf+(fat32_sec*SECTOR_SIZE));
 	printf("%s\n", fat->BS_OEMName);
 	printf("size: %d\n", fat->BPB_BytsPerSec);
 }
@@ -77,25 +80,42 @@ void init_fat()
                 FATSz = fat->BPB_FATSz32;
 
         RootDirSectors = (fat_table) + ((fat->BPB_RootEntCnt*32)+(fat->BPB_BytsPerSec-1))/fat->BPB_BytsPerSec;
+	FirstFatSector = fat_table + fat->BPB_ResvdSecCnt;
         FirstDataSector = fat->BPB_ResvdSecCnt+(fat->BPB_NumFATs * FATSz) + RootDirSectors;
 }
 
 static inline unsigned int get_sec(unsigned int cluster)
 {
-	return ((cluster - 2) * fat->BPB_SecPerClus) + FirstDataSector;
+        return ((cluster - 2) * fat->BPB_SecPerClus) + FirstDataSector;
+}
+
+unsigned int find_next_cluster(unsigned int currentry)
+{
+	unsigned int cluster = *(unsigned int*)(buf + (FirstFatSector * SECTOR_SIZE + currentry * 4));
+	return cluster;
 }
 
 void read_content(unsigned int clus)
 {
 	unsigned int sector = get_sec(clus);
-	unsigned char *text = buf+sector*512;
+	unsigned char *text = buf+sector*SECTOR_SIZE;
 	printf("%s", text);
+}
+
+void list_all_cluster(unsigned int first_clus)
+{
+	unsigned int next_clus = first_clus;
+	do {
+		printf("%d -> ", next_clus);
+		next_clus = find_next_cluster(next_clus);
+	} while (next_clus != 0x0FFFFFFF);
+	printf("end\n");
 }
 
 void read_file()
 {
 	unsigned int datasec = get_sec(2);
-	struct dir_entry *dir = (struct dir_entry*)(buf+(datasec*512));
+	struct dir_entry *dir = (struct dir_entry*)(buf+(datasec*SECTOR_SIZE));
 	unsigned int i = 0;
 
 	for (; i < 16; ++i) {
@@ -104,7 +124,9 @@ void read_file()
 		printf("Filename: %s\n", dir->DIR_Name);
 		unsigned int clus = (dir->DIR_FstClusHI << 16 | dir->DIR_FstClusLO);
 		printf("Size: %d\n", dir->DIR_FileSize);
-		read_content(clus);
+		printf("Data cluster: %d\n", clus);
+		list_all_cluster(clus);
+//		read_content(clus);
 		++dir;
 	}
 }
